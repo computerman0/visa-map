99792498258517bb175cf289de017075181aeac9
Ayden Mersich <111095231+computerman0@users.noreply.github.com>
2026-08-08 23:15:28 -0400

    Update next.config.js
    Setting up github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/visa-map',
+  images: { unoptimized: true },
   webpack(config) {
     config.experiments = { ...config.experiments, topLevelAwait: true }
     return config
